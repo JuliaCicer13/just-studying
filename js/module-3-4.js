060e4a30d35562cb8194b982f038cb689f46cac9
Yuliia <ciceron131313@gmail.com>
2025-08-26 02:53:56 +0300

    j

--- js/module-3-4.js
+++ js/module-3-4.js
@@ -1,13 +1,71 @@
+
+let styles = ["jazz", "blues"];
+styles.push("rock-n-roll");
+
+let index = styles.indexOf("blues");
+if (index !== -1) {
+    styles[index] = "classic";
+}
+
 function logItems(array){
-    let styles = [];
-    for (i = styles; i <= styles; i ++) {
-        styles.push('rock-n-roll');
-        if (styles.includes("blues")){
-           styles.toggle("classic");
-           return styles;
-        }
+    for (let i = 0; i < array.length; i++){
+        console.log(`${ i + 1} - ${array[i]}`)
     }
 }
 
 
-logItems(`1-jazz:${1-jazz}`, `2-blues: ${2-blues}`);
+// ..................
+
+function checkLogin(array) {
+     let name = prompt("Enter your name");
+    
+       if (logins.includes(name)) {
+        return alert(`Welcome, ${name}!`)
+       } else {
+          alert("User not found");
+       }
+     }
+
+ const logins = ["Peter", "John", "Igor", "Sasha"];
+ console.log(logins)
+//  .............................
+
+
+function caclculateAverage(arg){
+    let num = 0;
+    for ( i = num; i >= num; i++) {
+      if (arg === Number()) {
+      num += arg;
+    }  
+   return num / arg;
+    }
+}
+console.log(num)
+
+// .............................................
+
+let newArray = [];
+
+function calculateNum() {
+for (let array of newArray ) {
+    
+}
+
+}
+
+const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// ..........................
+
+function findSmallestNumber(numbers) {
+    for (i = numbers; i >= numbers; i++){
+         if (numbers === Array()) {
+        return Math.min(numbers)
+    } else {
+       alert ("Not a numbers");
+    }
+
+    }
+}
+const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+console.log(numbers);
